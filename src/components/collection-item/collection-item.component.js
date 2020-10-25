832d5a831ab4e1b17e02d720e72559a480ad67e9
Christine Fletcher <chrismis79@gmail.com>
2020-10-25 15:47:21 -0500

    styled preview and collection item

---
+++ src/components/collection-item/collection-item.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+
+        </div>
+    )
+}
+
+export default CollectionItem;
--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => {
     return (
@@ -8,9 +9,10 @@ const CollectionPreview = ({ title, items }) => {
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
                 {
-                    items.filter((item, idx) => idx < 4).map(item => (
-                        <div key={item.id}>{item.name}</div>
-                    ))
+                    items.filter((item, idx) => idx < 4)
+                        .map(({ id, ...otherItemProps }) => (
+                            <CollectionItem key={id} {...otherItemProps} />
+                        ))
                 }
             </div>
         </div>
