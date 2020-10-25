d725948e71ca9d0e27ec0f10c659a7ec1a87a633
Christine Fletcher <chrismis79@gmail.com>
2020-10-25 16:22:29 -0500

    added header component with svg logo and styled

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
+import Header from './components/header/header.component';
 
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header />
         <Switch>
           <Route exact path='/'>
             <HomePage />
